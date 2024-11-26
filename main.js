19f99062221c163cfa02b170fef1d56fc1f2387b
IzzatjonAkbarov <akbrv.i.006@gmail.com>
2024-11-26 12:08:47 +0500

    the project is done with responsive upto 280px

---
+++ main.js
@@ -0,0 +1,58 @@
+// // let width = prompt("enter width");
+// // let height = prompt("enter height");
+// // console.log(width * height);
+// // let sonnioshirish = prompt("enter number");
+// // console.log(sonnioshirish * sonnioshirish);
+// // let kwadratnihisoblash = prompt("enter number");
+// // console.log(kwadratnihisoblash * kwadratnihisoblash);
+// // let minutgaaylantiring = prompt("enter number");
+// // console.log(minutgaaylantiring * 60);
+// /*
+// data types
+// primitive and no primitve
+// primitive
+
+// string "'`fnivjwnfviujwnervwni`'"
+// number 12324354
+
+//  boolean true false
+//  symboll   uniqe malumot saqlaydi
+
+//  null have place but not placeholder
+//  undefined no place and place holder
+//   number symbol null undefined Bigint
+// nonprimitive or refernece
+// array[]
+//  object {}
+// */
+
+// let tugilganyil = prompt("yilni kiriting");
+// z = 2025 - tugilganyil;
+
+// let tugilganoy = prompt("oyni kiriting");
+// a = tugilganoy;
+// let arr = [
+//   "yanvar",
+//   "fevral",
+//   "mart",
+//   "aprel",
+//   "may",
+//   "iyun",
+//   "iyul",
+//   "avgust",
+//   "santabr",
+//   "oktabr",
+// ];
+// let tugilgankun = prompt("kun kiriting");
+// c = tugilgankun;
+
+// console.log(
+//   "davron raimjonov yoshingiz" +
+//     z +
+//     "da" +
+//     "tugilgan oy" +
+//     " " +
+//     arr[tugilganoy - 1] +
+//     "tugilgankun" +
+//     c
+// );
